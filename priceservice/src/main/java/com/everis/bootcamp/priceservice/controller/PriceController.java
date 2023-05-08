8eb7c1460282441500b10d29dd00aa508d4063c8
Alfonso Lobato Lopez <alobato1324@gmail.com>
2023-05-08 14:27:34 +0200

    EndPoints Apartado 1

--- priceservice/src/main/java/com/everis/bootcamp/priceservice/controller/PriceController.java
+++ priceservice/src/main/java/com/everis/bootcamp/priceservice/controller/PriceController.java
@@ -2,10 +2,10 @@ package com.everis.bootcamp.priceservice.controller;
 
 import java.util.List;
 
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.everis.bootcamp.priceservice.model.Prices;
@@ -21,14 +21,14 @@ public class PriceController {
 	}
 
 	// Petición de tipo GET para obtener el precio del elemento con el "id" especificado
-	@GetMapping(value= "/get-price/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(path= "/get-price/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Prices getPrice(@PathVariable Integer id) {
 		return priceService.get(id);
 		
 	}
 	
 	// Petición de tipo GET para obtener todos los precios
-	@GetMapping(value="/get-all-prices", produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(path="/get-all-prices", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Prices> getAllPrices() {
 		return priceService.getAllPrices();
 	}
