76b3d8d8640cf9891b6bc60720549576281c3e18
Alfonso Lobato Lopez <alobato1324@gmail.com>
2023-05-08 16:34:47 +0200

    Reto Final Terminado

--- reto-config-service/src/main/java/com/nttdata/bootcamp/retoconfigservice/controller/ConfigServiceController.java
+++ reto-config-service/src/main/java/com/nttdata/bootcamp/retoconfigservice/controller/ConfigServiceController.java
@@ -6,12 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ConfigServiceController {
+	
 
 	@Value("${reto.config-service}")
 	private String myConfig;
 	    
 	@GetMapping(path="/getConfig")
 	public String myConfig() {
+		
 	   	 return this.myConfig;
+	   	 
 	}
 }
